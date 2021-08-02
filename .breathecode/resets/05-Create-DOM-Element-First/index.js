82d2ac02f78e7b35e7d09c3da9524af21cda7d87
Moskzow <inmoskwetrust@gmail.com>
2021-08-02 15:48:45 +0000

    ex4 done

--- .breathecode/resets/05-Create-DOM-Element-First/index.js
+++ .breathecode/resets/05-Create-DOM-Element-First/index.js
@@ -4,8 +4,3 @@ document.querySelector("#myDiv").appendChild(elem);
 
 //Your code here
 // Don'change the code above
-
-let elem2 = document.createElement("p");
-elem2.innerHTML = "Hello World";
-elem2.style.background = "yellow";
-document.querySelector("#myDiv").appendChild(elem2);
